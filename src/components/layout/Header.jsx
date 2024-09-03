d0dc5a22ca0e662cfe3e4d6ea8e30c0121e6ddf1
DavidPensjo <david.pensjo@gmail.com>
2024-09-04 00:12:25 +0200

    Added Header bg color and made everything editable in Storyblok

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,25 +1,39 @@
 import React from "react";
+import { storyblokEditable } from "@storyblok/react";
 import Link from "next/link";
 
 function Header({ config }) {
   const headerBlocks = config.content.header;
 
-  const headerMenuBlock = headerBlocks.find(block => block.component === 'header_menu');
+  const headerMenuBlock = headerBlocks.find(
+    (block) => block.component === "header_menu"
+  );
   const header_menu = headerMenuBlock ? headerMenuBlock.header_menu : [];
 
-  const logoBlock = headerBlocks.find(block => block.component === 'logo');
+  const logoBlock = headerBlocks.find((block) => block.component === "logo");
   const logo = logoBlock ? logoBlock.logo : null;
 
+  const bgColorBlock = headerBlocks.find((block) => block.bg_color);
+  const bgColor = bgColorBlock ? bgColorBlock.bg_color : "bg-slate-300";
 
   return (
-    <header className="bg-slate-300">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
-        <Link href="#">
-          {logo && <img src={logo.filename} alt="logo" className="h-8 w-auto" />}
+    <header
+      style={{ backgroundColor: bgColor.value }}
+      {...storyblokEditable(bgColorBlock)}
+    >
+      <nav className="mx-auto flex items-center justify-between p-6 lg:px-8">
+        <Link href="#" {...storyblokEditable(logoBlock)}>
+          {logo && (
+            <img src={logo.filename} alt="logo" className="h-8 w-auto" />
+          )}
         </Link>
-        <ul className="flex">
+        <ul className="flex" {...storyblokEditable(headerMenuBlock)}>
           {header_menu.map((item) => (
-            <li key={item._uid} className="pr-5">
+            <li
+              key={item._uid}
+              className="pr-10 font-bold text-gray-50"
+              {...storyblokEditable(item)}
+            >
               <Link href={item.link.url}>{item.name}</Link>
             </li>
           ))}
